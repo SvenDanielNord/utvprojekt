0b84e174e77e61ef847def7cf5668a2ab8a63232
David <davnev0423@skola.goteborg.se>
2023-02-08 14:48:35 +0100

    Edit Game class

--- UtvProjekt/src/main/java/Game.java
+++ UtvProjekt/src/main/java/Game.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Game {
+    private String alphabet = "abcdefghijklmnopqrstuvwzyåäö";
+
+    public void playGame(Scanner input) {
+        System.out.println("Write alphabet");
+        long startTime = System.currentTimeMillis();
+        String answer = input.nextLine();
+        long stopTime = System.currentTimeMillis();
+        double usersTime = (stopTime - startTime) / 1000;
+
+        if (answer == alphabet) {
+            System.out.println("Answer is right. Your time is: " + usersTime);
+        }
+        else {
+            System.out.println("Answer  isn't right. Your answer is: " + answer + " and your time is: " + usersTime);
+        }
+    }
 }
