67e7280418ab5fe778b3ea38d610ccf973ca6724
Daniel Nord <svenor0701@skola.goteborg.se>
2023-02-08 13:22:16 +0100

    add java project

---
+++ UtvProjekt/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Scanner input = new Scanner(System.in);
+
+        // create player
+
+        // Game menu
+
+        System.out.println("1. Spela/n2 Highscore");
+        switch(input.nextInt()){
+
+            case 1:
+                //play game
+                break;
+            case 2:
+                // see highscore
+                break;
+
+                default:
+                    System.out.println("choose one or two");
+
+
+
+
+        }
+
+
+
+
+        // show highscore
+
+
+    }
+}
