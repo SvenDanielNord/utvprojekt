31bf3277478a307f20e7f82201e919f699ccdfe7
Daniel Nord <svenor0701@skola.goteborg.se>
2023-02-09 21:53:23 +0100

    add HighscoreTest

--- UtvProjekt/src/test/java/HigscoreTest.java
+++ UtvProjekt/src/test/java/HigscoreTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HigscoreTest {
+
+
+    private HighScore high0;
+    private HighScore high1;
+    private HighScore high2;
+
+    public HigscoreTest() {
+
+        high0 = new HighScore();
+        high1 = new HighScore("Mamma", 55);
+        high2 = new HighScore("Mamma", 55);
+        high0.addScoreToTable(high1);
+
+    }
+    //This test should throw toManyScoresException
+    @Test
+    public void testAddScoreToTable() {
+        try {
+            for (int i = 0; i < 40; i++) {
+                high0.addScoreToTable(high1);
+            }
+            high0.getNumberOfScores();
+            fail("för lite böcker för att kasta exception");
+        } catch (ToManyScoresException t) {
+            System.out.println("Yes jag fånga det");
+        }
+
+    }
+    @Test
+    public void test2EqualHighScores(){
+        assertEquals(high1,high2);
+    }
+
+
+
+
 }
