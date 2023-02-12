604d49e29b694876b1850cb1f6b6bb7058ccfc97
Daniel Nord <svenor0701@skola.goteborg.se>
2023-02-12 12:52:28 +0100

    Added test compareTo method

--- UtvProjekt/src/test/java/HigscoreTest.java
+++ UtvProjekt/src/test/java/HigscoreTest.java
@@ -8,12 +8,14 @@ public class HigscoreTest {
     private HighScore high0;
     private HighScore high1;
     private HighScore high2;
+    private HighScore high3;
 
     public HigscoreTest() {
 
         high0 = new HighScore();
         high1 = new HighScore("Mamma", 55);
         high2 = new HighScore("Mamma", 55);
+        high3 = new HighScore("Mamma", 3);
         high0.addScoreToTable(high1);
 
     }
@@ -31,11 +33,18 @@ public class HigscoreTest {
         }
 
     }
+    // Test if to Highscores i equal
     @Test
     public void test2EqualHighScores(){
         assertEquals(high1,high2);
     }
 
+
+    // Test compareTo method returns -1 if high3 time is faster than high2
+    @Test
+    public void testCompareTo(){
+        assertEquals(-1, high3.compareTo(high2));
+    }
 
 
 
