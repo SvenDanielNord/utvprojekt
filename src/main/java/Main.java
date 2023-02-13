8aa821fd2b038cfe58da6ca5eb89697f73b61a23
Daniel Nord <svenor0701@skola.goteborg.se>
2023-02-13 18:24:14 +0100

    uppdated output in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,32 +7,35 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Player player = new Player();
 
+        System.out.println("\n         Welcome to Alphabet Game! \n");
 
         // create player
         System.out.println("Write your name:");
         Player.name = input.nextLine();
-        System.out.println("Select language 1 for Swedish 2 English");
+        System.out.println("\nSelect language:\n1. Swedish\n2. English");
         Player.lang = input.nextInt();
-        // Game menu
+
 
         System.out.println();
         do {
-
+            // Game menu
             System.out.println("\n");
             System.out.println("          MENU");
-            System.out.println("       1. Play\n       2. High score\n       0. Exit");
+            System.out.println("       1. Play\n       2. High score\n       0. Exit\n");
 
             switch (Player.choice = input.nextInt()) {
 
                 case 1:
+                    //play game
                     String isCorrect = game.playGame(input, Player.lang);
                     if (isCorrect.equals("Corrext!")) {
                         HighScore highS = new HighScore(Player.name.toString(), game.getUsersTime());
                         highScore.addScoreToTable(highS);
                     }
+                    System.out.println("\n\nPress enter to continue");
                     input.nextLine();
 
-                    //play game
+
                     break;
                 case 2:
                     // see highScore
